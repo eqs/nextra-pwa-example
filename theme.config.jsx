3a27baf707ac9f46198826db2c0d8d15b03b7667
eqs <murashige.satoshi.mi1@gmail.com>
2023-09-06 21:37:47 +0900

    initial commit.

---
+++ theme.config.jsx
@@ -0,0 +1,11 @@
+export default {
+  logo: <strong>My Nextra Documentation</strong>,
+  useNextSeoProps: () => {
+    return {
+      titleTemplate: '%s – My Nextra Documentation'
+    }
+  },
+  project: {
+    link: 'https://github.com/eqs/nextra-pwa-example'
+  }
+}
